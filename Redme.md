3d387a99164b949dd7595370d3b2c8ea8c79c7a7
William <williambecker321@gmail.com>
2022-01-13 16:20:22 +0100

    found unice cars and amount and saved i arrayes

--- Redme.md
+++ Redme.md
@@ -1,1 +1,7 @@
 3.1 När js filen läsers in kör den javascripten automatiskt. Om det inte ligger i en funktion, då körs den när den blir kallad. 
+
+4.1 
+
+har bli märken (Array)
+
+map(kolla bilmärken,  unika=> ny array och hur mågna det är av varige sort.)
--- public/one.js
+++ public/one.js
@@ -4,3 +4,31 @@ var content = document.createElement('h2');
 content.innerText = 'One';
 
 document.getElementById('content').append(content);
+
+
+
+var cars = [];
+var unic = [];
+var amount = [];
+
+const myFunkrtion = () => {
+    cars.push(document.getElementById('carModle').value);
+
+    unic = [];
+    amount = [];
+
+    cars.forEach(car => {
+        if (!unic.includes(car)) {
+            var i = 0;
+            cars.forEach(element => {
+                if (element == car) {
+                    i++;
+                }
+            });
+            unic.push(car);
+            amount.push(i);
+        }
+    });
+}
+
+document.getElementById('addData').addEventListener('click', myFunkrtion)
